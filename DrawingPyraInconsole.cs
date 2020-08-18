139f36e0b2fd25b0d831a314abecdb9056faa6d7
Chaz Sherrill <chazsherrill@icloud.com>
2020-08-18 18:55:21 -0500

    adding the pyriamid

---
+++ DrawingPyraInconsole.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Continuing_with_Solo_Learn
+{
+    class Program
+    {
+        static void DrawPyramid(int n)
+        {
+            for (int i = 1; i<=n; i++)
+            {
+                for (int j = i; j <= n; j++)
+                {
+                    Console.Write("  ");
+                }
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*"+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            DrawPyramid(10);
+        }
+    }
+}
+//making a pyramid
